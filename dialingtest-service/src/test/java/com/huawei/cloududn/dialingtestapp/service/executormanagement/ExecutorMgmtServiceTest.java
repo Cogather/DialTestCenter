d6019beb839dc36370fd91f4052a106aec41075d
LuJingl30079059 <lujing63@h-partners.com>
2025-11-18 11:42:37 +0800

    解决报错

--- dialingtest-service/src/test/java/com/huawei/cloududn/dialingtestapp/service/executormanagement/ExecutorMgmtServiceTest.java
+++ dialingtest-service/src/test/java/com/huawei/cloududn/dialingtestapp/service/executormanagement/ExecutorMgmtServiceTest.java
@@ -20,6 +20,7 @@ import org.junit.runner.RunWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.time.Instant;
@@ -29,14 +30,17 @@ import java.util.List;
 import javax.websocket.Session;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.clearInvocations;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoInteractions;
